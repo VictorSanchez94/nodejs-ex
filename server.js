9f3285917020895f8a048fd1bb423eaed7549485
VictorSanchez94 <vs_zgz94@hotmail.com>
2016-11-04 22:37:53 +0100

    API

--- server.js
+++ server.js
@@ -108,8 +108,8 @@ app.get('/requestToInit', function (req, res) {
   }
 });
 
-app.post('/updateState/:id', function (req, res) {
-  if (req.params.id == 1) {   //Player 1
+app.post('/updateState', function (req, res) {
+  if (req.id == 1) {   //Player 1
     /*gameInfo1.offers = req.offers;
     if (gameInfo1.offers === 0 && winner === -1) {
       winner = 2;
